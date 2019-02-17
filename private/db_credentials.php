c69d0a04dd24d389ab3c437e63046ac964dd6515
Hayelom Reda <rhayelom@gmail.com>
2019-02-17 12:51:16 -0700

    minor fix

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -2,7 +2,7 @@
 define("DB_SERVER", "3306");
 define("DB_USER", "lhsbz5crfzusmwag");
 define("DB_PASS", "wyl5adec8otkm9dt");
-define("DB_NAME", "	ge7swizm5mv7k3qa");
+define("DB_NAME", "ge7swizm5mv7k3qa");
 
 
 $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
