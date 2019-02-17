00a041c334b96e8fb51ce0ead3c17e5678d6668f
Hayelom Reda <rhayelom@gmail.com>
2019-02-17 14:53:35 -0700

    some change

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,8 +1,8 @@
-<!-- <?php
-// define("DB_SERVER", "us-cdbr-iron-east-03.cleardb.net");
-// define("DB_USER", "b5ea288b7011a6");
-// define("DB_PASS", "65e5c6d2");
-// define("DB_NAME", "heroku_8965ed27e5c21a3");
+<?php
+define("DB_SERVER", "us-cdbr-iron-east-03.cleardb.net");
+define("DB_USER", "b5ea288b7011a6");
+define("DB_PASS", "65e5c6d2");
+define("DB_NAME", "heroku_8965ed27e5c21a3");
 
 
 $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
@@ -13,4 +13,3 @@ if(! $connection ) {
 }
 
 ?>
- -->
