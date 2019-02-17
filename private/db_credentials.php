5ce1550afc3c03e7c77816e624aa4b5081aca4b9
Hayelom Reda <rhayelom@gmail.com>
2019-02-17 14:20:37 -0700

    some change

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,26 +1,15 @@
 <?php
-$url = getenv('mysql://lhsbz5crfzusmwag:wyl5adec8otkm9dt@blonze2d5mrbmcgf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ge7swizm5mv7k3qa
+define("DB_SERVER", "us-cdbr-iron-east-03.cleardb.net");
+define("DB_USER", "b5ea288b7011a6");
+define("DB_PASS", "65e5c6d2");
+define("DB_NAME", "heroku_8965ed27e5c21a3");
 
-');
-$dbparts = parse_url($url);
 
-$hostname = $dbparts['blonze2d5mrbmcgf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'];
-$username = $dbparts['lhsbz5crfzusmwag'];
-$password = $dbparts['wyl5adec8otkm9dt'];
-$database = ltrim($dbparts['path'],'/');
-// $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
-// // echo "You are connected";
-//                   // Check connection
-// if(! $connection ) {
-// 	die('Could not connect: ' . mysqli_error());
-// }
-// Create connection
-$conn = mysqli_connect($hostname, $username, $password, $database);
-
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+// echo "You are connected";
+                  // Check connection
+if(! $connection ) {
+	die('Could not connect: ' . mysqli_error());
 }
-echo "Connection was successfully established!";
-?>
 
+?>
